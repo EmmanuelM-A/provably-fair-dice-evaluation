61084cf77d950339e768cf238da272f73554929e
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2026-05-11 07:18:22 +0100

    feat: added the boilerplate code for the implementations for ChainLinkVRF and Drand mechanisms

---
+++ src/provably_fair_mechanisms/drand_mechanism.py
@@ -0,0 +1,31 @@
+from src.enigines.randomness import RandomnessEngine
+from src.enigines.verification import VerificationEngine
+from src.utils.types import RollRecord, VerificationResult
+
+
+class DrandMechanism(RandomnessEngine, VerificationEngine):
+
+    # ========================= MECHANISM SPECIFIC =========================
+
+    # ======================== RANDOMNESS OPERATIONS ========================
+
+    def get_entropy(self) -> bytes:
+        pass
+
+    def get_noise(self) -> bytes:
+        pass
+
+    def get_drbg_instance(self) -> any:
+        pass
+
+    def generate_roll(
+        self, server_seed: str, client_seed: str, nonce: str = ""
+    ) -> RollRecord:
+        pass
+
+    # ======================= VERIFICATION OPERATIONS =======================
+
+    def verify(
+        self, record: RollRecord, disclosed_server_seed: str
+    ) -> VerificationResult:
+        pass
