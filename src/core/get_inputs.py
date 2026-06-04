5e5a6f3865f1bb4f2af177d4ac2063fa9dccd617
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2026-06-04 21:55:44 +0100

    tidying up code

--- src/core/get_inputs.py
+++ src/core/get_inputs.py
@@ -11,25 +11,6 @@ from src.provably_fair_mechanisms.chainlink_vrf_mechanism import betswirl_dice_o
 from src.provably_fair_mechanisms.drand_mechanism import blockrand_dice_outcome
 from src.provably_fair_mechanisms.hmac_mechanism import StakesHMACMechanism, stake_dice_outcome
 
-"""
-Script to run system:
-
---c COUNT: Number of rolls to generate. Default is 1000
---t TIER: Evaluation depth tier (LIGHT, IN_DEPTH, FULL_DEPTH). Default is LIGHT.
---d path/to/rolls.csv: The file path to save generated rolls. REQUIRED
---r path/to/results.json: The file path to save evaluation results. REQUIRED
---np path/to/nper.json: The file path to collected nper values REQUIRED
-
-
-generate_rolls: python -m src.core.rolls --c COUNT --d DATA_PATH
-
-evaluate: python -m src.core.evaluate --r RESULTS_PATH --t TIER --d DATA_PATH
-
-report: python -m src.core.report --r RESULTS_PATH --np NPER_PATH
-
-run_all: python -m src.core.run_all --c COUNT --t TIER --d DATA_PATH --r RESULTS_PATH --np NPER_PATH
-
-"""
 
 def get_mechanism_mapping_fn():
     """
